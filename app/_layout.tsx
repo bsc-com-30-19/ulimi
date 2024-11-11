8036e296cb63c840308ac8f6db72fe5c0423a22d
bsc-com-30-19 <bsc-com-30-19@unima.ac.mw>
2024-11-11 11:01:01 +0200

    switching to main

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,10 +3,7 @@ import { Slot, Stack } from 'expo-router'
 
 const RootLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="Crops" options={{title: "String"}} />
-      <Stack.Screen name="Crops" options={{title: "String"}} />
-    </Stack>
+    <Slot/>
   )
 }
 
