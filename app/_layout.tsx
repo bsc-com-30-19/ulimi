ee979775868088784d08380f18682feccdc75491
“fortuneMphalo” <bed-com-22-21@unima.ac.mw>
2024-11-11 23:03:24 +0200

    Change of directory of the farmmanagement file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,37 @@
-import { Slot } from 'expo-router'
-import { StyleSheet, Text, View } from 'react-native'
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
 
+import { useColorScheme } from '@/hooks/useColorScheme';
 
-const RootLayout = () => {
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <Slot />
-  )
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(crops)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+    </ThemeProvider>
+  );
 }
-
-export default RootLayout
-
-const styles = StyleSheet.create({})
