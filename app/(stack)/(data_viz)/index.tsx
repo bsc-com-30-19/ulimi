bc7f6cf8be39c869a34e5a2c7444ed4b57b6cae4
bsc-com-30-19 <bsc-com-30-19@unima.ac.mw>
2024-11-14 23:41:29 +0200

    Worked on making a sidebar, so far a lot of struggle has gone into making it work

---
+++ app/(stack)/(data_viz)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, StyleSheet, View } from 'react-native';
+
+export default function DataScreen() {
+  return (
+    <View>
+      <Text className="text-lg">Data visualisation</Text>
+    </View>
+  );
+}
