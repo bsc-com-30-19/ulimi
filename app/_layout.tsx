f91a9e17807ce7e6bf2847b9c1269d421f0d8513
bsc-com-30-19 <bsc-com-30-19@unima.ac.mw>
2024-11-21 17:27:28 +0200

    Fixed routing issue from main menu to other pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,7 @@ import { Stack } from 'expo-router';
 const StackExpo = () =>{
   return(
     <Stack >
-      <Stack.Screen name="Index"/>
+      <Stack.Screen name="Index" options={{headerShown:false}}/>
       <Stack.Screen name="(home)" options={{headerShown:false}}/>
       <Stack.Screen name="(crops)" />
       <Stack.Screen name="(weather)" />
--- components/menus/MenuBlock.tsx
+++ components/menus/MenuBlock.tsx
@@ -1,13 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import React from 'react'
 import { Href, Link } from 'expo-router';
 
 const MenuBlock = ({name, link, OnPress}:{name?:string; link:any, OnPress?:any}) => {
   return (
-    <View  className='h-56 w-44 bg-[#D9D9D9] rounded-2xl m-2'>
-      <Link  onPress={OnPress} href={link} style={{height:'100%', width:'100%', position:'absolute'} }/>
-      <Text className='text-black mt-48 ml-6'>{name}</Text>
-    </View>
+    
+      <Link className='m-2' push href={link} asChild>
+        <Pressable className='h-56 w-44 bg-[#D9D9D9] rounded-2xl '>
+          <Text className='text-black mt-48 ml-6'>{name}</Text>
+        </Pressable>
+      </Link>
+    
   )
 }
 
