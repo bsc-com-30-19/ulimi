ada24343a2da9d2b8f0c27f8ce5d3556db01e954
bsc-com-30-19 <bsc-com-30-19@unima.ac.mw>
2024-11-22 11:37:09 +0200

    Removed all index files in all other routes.... will have to deal with merge conflicts later in the road

--- app/(data_viz)/_layout.tsx
+++ app/(data_viz)/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Data Visualisation' }} />
+      <Stack.Screen name="dataScreen" options={{ title: 'Data Visualisation' }} />
     </Stack>
   );
 }
