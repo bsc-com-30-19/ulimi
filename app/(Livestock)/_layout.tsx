310e146dd35a204873a26faa739744d4d0dabeca
bsc-com-30-19 <bsc-com-30-19@unima.ac.mw>
2024-11-27 08:56:28 +0200

    Trying to merge Fortune's work

--- app/(Livestock)/_layout.tsx
+++ app/(Livestock)/_layout.tsx
@@ -3,9 +3,10 @@ import { Stack } from 'expo-router'
 const Livestock = () => {
   return (
     <Stack>
-      <Stack.Screen name="liveStockMenu" options={{headerShown: false,title:"Livestock"}}/>
-      <Stack.Screen name="ProducePage" options={{headerShown: false,title:"Produce"}}/>
-      <Stack.Screen name="TaskPage" options={{headerShown: false,title:"Tasks"}}/>
+      <Stack.Screen name="liveStockMenu" options={{title:"Livestock"}}/>
+      <Stack.Screen name="LivestockPage" options={{title:"Livestock"}}/>
+      <Stack.Screen name="ProducePage" options={{title:"Produce"}}/>
+      <Stack.Screen name="TaskPage" options={{title:"Tasks"}}/>
     </Stack>
   )
 }
