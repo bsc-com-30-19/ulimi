8941a58b65732511657d11c22fc24941899c379e
bsc-com-30-19 <bsc-com-30-19@unima.ac.mw>
2024-11-27 08:21:54 +0200

    Added stacks to Jessie's screen

--- app/(Livestock)/_layout.tsx
+++ app/(Livestock)/_layout.tsx
@@ -4,7 +4,8 @@ const Livestock = () => {
   return (
     <Stack>
       <Stack.Screen name="liveStockMenu" options={{headerShown: false,title:"Livestock"}}/>
-
+      <Stack.Screen name="ProducePage" options={{headerShown: false,title:"Produce"}}/>
+      <Stack.Screen name="TaskPage" options={{headerShown: false,title:"Tasks"}}/>
     </Stack>
   )
 }
